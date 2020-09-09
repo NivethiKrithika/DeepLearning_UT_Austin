d5fb2b3e39e5442f7f6bf4be5ffcba567d7bf00d
NKrithika <53506588+NivethiKrithika@users.noreply.github.com>
2020-09-09 20:11:15 +0530

    Update logging.py

--- homework2/homework/logging.py
+++ homework2/homework/logging.py
@@ -1,7 +1,7 @@
 from os import path
 import torch
 import torch.utils.tensorboard as tb
-
+import numpy
 
 def test_logging(train_logger, valid_logger):
 
@@ -12,19 +12,29 @@ def test_logging(train_logger, valid_logger):
     Make sure to set global_step correctly, for epoch=0, iteration=0: global_step=0
     Call the loss 'loss', and accuracy 'accuracy' (no slash or other namespace)
     """
-
+    train_global_step = 0
     # This is a strongly simplified training loop
-    for epoch in range(10):
+    for epoch in range(20):
         torch.manual_seed(epoch)
+        accu = []
         for iteration in range(20):
             dummy_train_loss = 0.9**(epoch+iteration/20.)
             dummy_train_accuracy = epoch/10. + torch.randn(10)
-            raise NotImplementedError('Log the training loss')
-        raise NotImplementedError('Log the training accuracy')
+            accu.append(dummy_train_accuracy.mean().item())
+            train_logger.add_scalar('loss', dummy_train_loss, global_step = train_global_step)
+            train_global_step += 1
+            #raise NotImplementedError('Log the training loss')
+        train_logger.add_scalar('accuracy',numpy.array(accu).mean(),global_step = train_global_step)
+        #raise NotImplementedError('Log the training accuracy')
         torch.manual_seed(epoch)
+        accu_valid = []
         for iteration in range(10):
             dummy_validation_accuracy = epoch / 10. + torch.randn(10)
-        raise NotImplementedError('Log the validation accuracy')
+            accu_valid.append(dummy_validation_accuracy.mean().item())
+        
+        valid_logger.add_scalar('accuracy',numpy.array(accu_valid).mean(),global_step = train_global_step)
+        #raise NotImplementedError('Log the validation accuracy')
+
 
 
 if __name__ == "__main__":
