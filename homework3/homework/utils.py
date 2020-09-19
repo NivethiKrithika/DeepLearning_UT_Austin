a639a0ffc52fcbe1f2aa7178b1d3fe29ac3d0eb7
NKrithika <53506588+NivethiKrithika@users.noreply.github.com>
2020-09-19 08:05:54 +0530

    Update utils.py

--- homework3/homework/utils.py
+++ homework3/homework/utils.py
@@ -23,8 +23,8 @@ class SuperTuxDataset(Dataset):
     def __init__(self, dataset_path):
         i = 1
         #print(transform)
-        self.transform = torchvision.transforms.Compose([torchvision.transforms.Resize((128,128)),
-                                                         torchvision.transforms.RandomResizedCrop(64),
+        self.transform = torchvision.transforms.Compose([torchvision.transforms.Resize((88,88)),
+                                                         torchvision.transforms.RandomResizedCrop(24),
                                                          torchvision.transforms.RandomHorizontalFlip(),
                                                        torchvision.transforms.ToTensor()])
         #tr_image = torchvision.transforms.ToTensor()
