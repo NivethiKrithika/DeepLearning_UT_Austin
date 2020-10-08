b110d96d1a2e70ac0a825b24db4b8de40f6a5129
NKrithika <53506588+NivethiKrithika@users.noreply.github.com>
2020-10-08 12:50:49 +0530

    Update utils.py

--- homework4/homework/utils.py
+++ homework4/homework/utils.py
@@ -10,7 +10,9 @@ class DetectionSuperTuxDataset(Dataset):
         self.files = []
         for im_f in glob(path.join(dataset_path, '*_im.jpg')):
             self.files.append(im_f.replace('_im.jpg', ''))
-        self.transform = transform
+        self.transform = dense_transforms.Compose([dense_transforms.ColorJitter(brightness=0.3, contrast=0.4, saturation=0.2, hue=0.1),
+                                                        dense_transforms.RandomHorizontalFlip(),
+                                                        dense_transforms.ToTensor()])
         self.min_size = min_size
 
     def _filter(self, boxes):
