83c4e6f2d938187505e92477ceaf7415fa2c3c9b
NKrithika <53506588+NivethiKrithika@users.noreply.github.com>
2020-10-20 01:35:02 +0530

    Update models.py

--- homework3/homework/models.py
+++ homework3/homework/models.py
@@ -72,6 +72,7 @@ class FCN(torch.nn.Module):
             self.concat_layers = torch.nn.Sequential(torch.nn.Conv2d(in_channels, out_channels,3,padding = 1,stride = 1),
                                                      torch.nn.BatchNorm2d(out_channels),
                                                      torch.nn.ReLU(),
+                                                     torch.nn.Dropout(p = 0.1),
                                                      torch.nn.Conv2d(out_channels,out_channels,3,padding = 1,stride = 1),
                                                      torch.nn.BatchNorm2d(out_channels),
                                                      torch.nn.ReLU())
