357ee77ab6a1f5f1cb9c29ed83632f4f1ed082e5
NKrithika <53506588+NivethiKrithika@users.noreply.github.com>
2020-11-11 20:42:32 +0530

    Update utils.py

--- homework5/homework/utils.py
+++ homework5/homework/utils.py
@@ -16,7 +16,7 @@ class SuperTuxDataset(Dataset):
         from glob import glob
         from os import path
         self.data = []
-        for f in glob(path.join(dataset_path, '*.csv')):
+        for f in sorted(glob(path.join(dataset_path, '*.csv'))):
             i = Image.open(f.replace('.csv', '.png'))
             i.load()
             self.data.append((i, np.loadtxt(f, dtype=np.float32, delimiter=',')))
