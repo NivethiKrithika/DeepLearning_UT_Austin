347b2ee43a3740180efdd0b2495d6d966968f5a3
NKrithika <53506588+NivethiKrithika@users.noreply.github.com>
2020-11-12 10:35:02 +0530

    Update planner.py

--- homework5/homework/planner.py
+++ homework5/homework/planner.py
@@ -91,7 +91,7 @@ class Planner(torch.nn.Module):
             # Add the skip connection
             if self.use_skip:
                 z = torch.cat([z, up_activation[i]], dim=1)
-        return spatial_argmax(self.classifier(z))
+        return spatial_argmax(sef.sig_layer(self.classifier(z)))
         
 
 def save_model(model):
