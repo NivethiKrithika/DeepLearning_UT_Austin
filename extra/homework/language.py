9e7ad2f17ca81fa767c49fe9c354bc0e3fd69214
NKrithika <53506588+NivethiKrithika@users.noreply.github.com>
2020-11-17 13:47:34 +0530

    Update language.py

--- extra/homework/language.py
+++ extra/homework/language.py
@@ -100,6 +100,23 @@ class TopNHeap:
 
 
 def beam_search(model: LanguageModel, beam_size: int, n_results: int = 10, max_length: int = 100, average_log_likelihood: bool = False):
+    
+    strings =[]
+    log_likel = []
+    h = TopNHeap(n_results)
+    for i in range(30):
+      string1 = sample_random(model,max_length)
+      print(string1)
+      strings.append(string1)
+      log_like = log_likelihood(model,string1)
+      if(average_log_likelihood == True):
+        log_like = log_like/len(string1)
+      log_likel.append(log_like.item())
+      h.add(log_like.item())
+    print("h elements are {}".format(h.elements))
+    print("the strings are {}".format(strings))
+    print("likelihood is {}".format(log_likel))
+
     """
     Your code here
 
